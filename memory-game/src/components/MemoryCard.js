185c4ebffdaa217270938a2b156d28dc31af901e
Enoch <enoch4kigwila@gmail.com>
2022-05-11 10:40:54 -0400

    added 16 cards

---
+++ memory-game/src/components/MemoryCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './MemoryCard.css';
+
+class MemoryCard extends React.Component {
+
+    render(){
+        return (
+            // <h1 style={{color:'black'}}> CARD</h1>
+            <div className='MemoryCard'>
+                <img src='https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png'/>
+            </div>
+            
+        )
+    }
+}
+
+export default MemoryCard;
